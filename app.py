f0b76dda4d22ff2bccfb5413c1c2a481eb7205ce
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2025-02-01 23:44:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def main():
     if uploaded_file is not None:
         # Open the uploaded image using PIL
         image = Image.open(uploaded_file).convert("RGB")
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
         st.write("Extracting labels, please wait...")
 
         # Load the pre-trained model
